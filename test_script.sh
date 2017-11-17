7f21c74577a80b3a7346242ee404e7b544d6a754
Azhor Ahai <debapriya.das.blr@gmail.com>
2017-11-17 06:44:00 +0530

    test script more verbose

--- test_script.sh
+++ test_script.sh
@@ -5,9 +5,11 @@ echo "hello, $USER. I wish to test the code"
 echo "current directory: $PWD"
 
 set -e
+echo "Cleaning the binaries : "
 make clean #cleaning
-
+echo "Compiling : "
 make ||  exit
 
+echo "Compile done, now running the test cases : "
 chmod +x mytunes
 ./mytunes
